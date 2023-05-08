f0118d98f324bae23384e3d9c3f8ae89f46a18c2
alexandre <alexandre.bbch@eduge.ch>
2023-05-08 09:34:50 +0200

    ajout de l'algorithme de trie en shell avec visuel

--- Code/AlgoTri/AlgoTri/FrmShell.Designer.cs
+++ Code/AlgoTri/AlgoTri/FrmShell.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmShell";
+            components = new System.ComponentModel.Container();
+            rbStepByStep = new RadioButton();
+            rbFast = new RadioButton();
+            rbVerySlow = new RadioButton();
+            rbNormal = new RadioButton();
+            rbSlow = new RadioButton();
+            gbSpeedAnimation = new GroupBox();
+            buttonStartSort = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            panelResultat = new Panel();
+            gbSpeedAnimation.SuspendLayout();
+            SuspendLayout();
+            // 
+            // rbStepByStep
+            // 
+            rbStepByStep.AutoSize = true;
+            rbStepByStep.Checked = true;
+            rbStepByStep.Location = new Point(6, 22);
+            rbStepByStep.Name = "rbStepByStep";
+            rbStepByStep.Size = new Size(73, 19);
+            rbStepByStep.TabIndex = 2;
+            rbStepByStep.TabStop = true;
+            rbStepByStep.Text = "Pas à pas";
+            rbStepByStep.UseVisualStyleBackColor = true;
+            // 
+            // rbFast
+            // 
+            rbFast.AutoSize = true;
+            rbFast.Location = new Point(6, 122);
+            rbFast.Name = "rbFast";
+            rbFast.Size = new Size(61, 19);
+            rbFast.TabIndex = 6;
+            rbFast.Text = "Rapide";
+            rbFast.UseVisualStyleBackColor = true;
+            // 
+            // rbVerySlow
+            // 
+            rbVerySlow.AutoSize = true;
+            rbVerySlow.Location = new Point(6, 47);
+            rbVerySlow.Name = "rbVerySlow";
+            rbVerySlow.Size = new Size(68, 19);
+            rbVerySlow.TabIndex = 3;
+            rbVerySlow.Text = "Très lent";
+            rbVerySlow.UseVisualStyleBackColor = true;
+            // 
+            // rbNormal
+            // 
+            rbNormal.AutoSize = true;
+            rbNormal.Location = new Point(6, 97);
+            rbNormal.Name = "rbNormal";
+            rbNormal.Size = new Size(65, 19);
+            rbNormal.TabIndex = 5;
+            rbNormal.Text = "Normal";
+            rbNormal.UseVisualStyleBackColor = true;
+            // 
+            // rbSlow
+            // 
+            rbSlow.AutoSize = true;
+            rbSlow.Location = new Point(6, 72);
+            rbSlow.Name = "rbSlow";
+            rbSlow.Size = new Size(48, 19);
+            rbSlow.TabIndex = 4;
+            rbSlow.Text = "Lent";
+            rbSlow.UseVisualStyleBackColor = true;
+            // 
+            // gbSpeedAnimation
+            // 
+            gbSpeedAnimation.Controls.Add(rbStepByStep);
+            gbSpeedAnimation.Controls.Add(rbFast);
+            gbSpeedAnimation.Controls.Add(rbVerySlow);
+            gbSpeedAnimation.Controls.Add(rbNormal);
+            gbSpeedAnimation.Controls.Add(rbSlow);
+            gbSpeedAnimation.Location = new Point(12, 139);
+            gbSpeedAnimation.Name = "gbSpeedAnimation";
+            gbSpeedAnimation.Size = new Size(193, 154);
+            gbSpeedAnimation.TabIndex = 10;
+            gbSpeedAnimation.TabStop = false;
+            gbSpeedAnimation.Text = "Vitesse d'éxécution";
+            // 
+            // buttonStartSort
+            // 
+            buttonStartSort.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonStartSort.Location = new Point(498, 392);
+            buttonStartSort.Name = "buttonStartSort";
+            buttonStartSort.Size = new Size(279, 46);
+            buttonStartSort.TabIndex = 13;
+            buttonStartSort.Text = "Trie";
+            buttonStartSort.UseVisualStyleBackColor = true;
+            buttonStartSort.Click += buttonStartSort_Click;
+            // 
+            // panelResultat
+            // 
+            panelResultat.Location = new Point(211, 12);
+            panelResultat.Name = "panelResultat";
+            panelResultat.Size = new Size(830, 361);
+            panelResultat.TabIndex = 14;
+            // 
+            // FrmShell
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1053, 450);
+            Controls.Add(panelResultat);
+            Controls.Add(buttonStartSort);
+            Controls.Add(gbSpeedAnimation);
+            Name = "FrmShell";
+            Text = "FrmComb";
+            gbSpeedAnimation.ResumeLayout(false);
+            gbSpeedAnimation.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+        private RadioButton rbStepByStep;
+        private RadioButton rbFast;
+        private RadioButton rbVerySlow;
+        private RadioButton rbNormal;
+        private RadioButton rbSlow;
+        private GroupBox gbSpeedAnimation;
+        private Button buttonStartSort;
+        private System.Windows.Forms.Timer timer1;
+        private Panel panelResultat;
     }
 }
--- Code/AlgoTri/AlgoTri/FrmShell.cs
+++ Code/AlgoTri/AlgoTri/FrmShell.cs
@@ -1,20 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace AlgoTri
 {
     public partial class FrmShell : Form
     {
+        int[] tab = Enumerable.Range(1, 20).OrderBy(x => Guid.NewGuid()).Take(20).ToArray();
         public FrmShell()
         {
             InitializeComponent();
         }
+
+        private void buttonStartSort_Click(object sender, EventArgs e)
+        {
+            ShellSort();
+        }
+        private void ShellSort()
+        {
+            int n = tab.Length;
+            int gap = n / 2;
+            Graphics g = panelResultat.CreateGraphics();
+
+            while (gap > 0)
+            {
+                for (int i = gap; i < n; i++)
+                {
+                    int temp = tab[i];
+
+                    int j;
+                    for (j = i; j >= gap && tab[j - gap] > temp; j -= gap)
+                    {
+                        tab[j] = tab[j - gap];
+                        DisplayElements(g);
+                        Application.DoEvents();
+                        System.Threading.Thread.Sleep(500);
+                    }
+
+                    tab[j] = temp;
+                }
+
+                gap /= 2;
+            }
+
+            DisplayElements(g);
+        }
+
+        private void DisplayElements(Graphics g)
+        {
+            int rectWidth = 30;
+            int rectHeightFactor = 10;
+            int rectSpacing = 10;
+            int rectXOffset = 20;
+            int rectYOffset = 20;
+
+            g.Clear(Color.White);
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                int rectHeight = tab[i] * rectHeightFactor;
+                int rectX = rectXOffset + i * (rectWidth + rectSpacing);
+                int rectY = rectYOffset + (200 - rectHeight);
+
+                Rectangle rect = new Rectangle(rectX, rectY, rectWidth, rectHeight);
+                g.DrawRectangle(Pens.Black, rect);
+                g.DrawString(tab[i].ToString(), Font, Brushes.Black, rectX, rectY + rectHeight + 5);
+            }
+        }
+
+
     }
 }
